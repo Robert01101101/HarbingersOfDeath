e47b4e613124e8a0c3f10f6f266d1f462b0e394c
robert-michels <rmichels@sfu.ca>
2020-10-06 20:33:49 -0700

    Stylized & accessible dropdowns (select element) done.

---
+++ src/Util/SelectInputs.php
@@ -0,0 +1,62 @@
+<?php
+namespace Util\HTML;
+
+/**
+ * Class SelectInputs
+ * @package Util\HTML
+ *
+ * Creates SelectInputs and adds them to the page
+ * Note by Robert: Feel free to improve on, using a bunch of echo commands seems wrong but I had to get it done fast.
+ * TODO: Update days based on month selected, or do validation at the very least.
+ */
+class SelectInputs {
+
+    
+
+    //Create a dropdown with number values (specify start, end, label of the dropdown & ID for Aria)
+    public static function numberInput($start, $end, $label, $id){
+        echo '<div class="form__cell">';
+            echo '<label id="'.$id.'_Label" class="input__label input__label--small" for="'.$id.'">'.$label.'</label><br>';
+            echo '<div class="select">';
+                echo '<div class="selectWrapper">';
+                    echo '<select id="'.$id.'" name="'.$id.'" class="selectNative js-selectNative" aria-labelledby="'.$id.'_Label">';
+                        echo '<option value="sel" disabled="" selected="">'.$label.'</span>';
+                        for ($x = $start; $x <= $end; $x++) {
+                            echo '<option value="'.$x.'">'.$x.'</option>'."\n";
+                        }
+                    echo '</select>';
+                    echo '<div class="selectCustom js-selectCustom" aria-hidden="true">';
+                        echo '<div class="selectCustom-trigger">'.$label.'</div>';
+                            echo '<div class="selectCustom-options">';
+                            for ($x = $start; $x <= $end; $x++) {
+                                echo '<div class="selectCustom-option" data-value="'.$x.'">'.$x.'</div>'."\n";
+                            }
+        echo '</div></div></div></div></div>';
+    }
+
+    //Create a dropdown with month values
+    public static function monthInput(){
+        $months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        $label = "Month";
+        $id = "birthday__months";
+        $length = count($months);
+
+        echo '<div class="form__cell">';
+            echo '<label id="'.$id.'_Label" class="input__label input__label--small" for="'.$id.'">'.$label.'</label><br>';
+            echo '<div class="select">';
+                echo '<div class="selectWrapper">';
+                    echo '<select id="'.$id.'" name="'.$id.'" class="selectNative js-selectNative" aria-labelledby="'.$id.'_Label">';
+                        echo '<option value="sel" disabled="" selected="">'.$label.'</span>';
+                        for ($x = 0; $x < $length; $x++) {
+                            echo '<option value="'.$months[$x].'">'.$months[$x].'</option>'."\n";
+                        }
+                    echo '</select>';
+                    echo '<div class="selectCustom js-selectCustom" aria-hidden="true">';
+                        echo '<div class="selectCustom-trigger">'.$label.'</div>';
+                            echo '<div class="selectCustom-options">';
+                            for ($x = 0; $x < $length; $x++) {
+                                echo '<div class="selectCustom-option" data-value="'.$months[$x].'">'.$months[$x].'</div>'."\n";
+                            }
+        echo '</div></div></div></div></div>';
+    }
+}
